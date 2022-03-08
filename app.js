477fbab08ccd51bfb7422ac74924040d777b929b
Nasir <nasir-islam@hotmail.com>
2022-03-08 17:13:59 +0000

    log all required details of flights

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ const renderData = async () => {
     await loadData();   // Need await to ensure it doesn't move on until it loads data
     console.log(allLaunches);
 
+    console.log(allLaunches[0].name)
     console.log(allLaunches[0].flight_number)
+    console.log(allLaunches[0].date_utc)
+    console.log(allLaunches[0].date_utc.split('T')[0])
+    console.log(allLaunches[0].links.patch.small)
+    console.log(allLaunches[0].details)
+    console.log(allLaunches[0].links.article)
 
 
 }
