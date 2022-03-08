e9a93265361550fddf219359f00c51ea6a82e97f
Nasir <nasir-islam@hotmail.com>
2022-03-08 17:34:31 +0000

    render one flight

--- app.js
+++ app.js
@@ -21,6 +21,42 @@ const renderData = async () => {
     console.log(allLaunches[0].details)
     console.log(allLaunches[0].links.article)
 
+    // Let's try create and append to all-launch-container
+    allLaunchContainer = document.querySelector("#all-launch-container");
+    
+    const launch = document.createElement('section');
+    launch.classList.add('launch');
+
+    const launchName = document.createElement('h2');
+    launchName.innerText = allLaunches[0].name
+    launch.appendChild(launchName)
+
+    const launchNumber = document.createElement('h3');
+    launchNumber.innerText = allLaunches[0].flight_number
+    launch.appendChild(launchNumber)
+
+    const launchDate = document.createElement('h3');
+    launchDate.innerText = allLaunches[0].date_utc.split('T')[0]
+    launch.appendChild(launchDate)
+
+    const launchImg = document.createElement('img');
+    launchImg.src = allLaunches[0].links.patch.small
+    launch.appendChild(launchImg)
+
+    const launchDetailTitle = document.createElement('p');
+    launchDetailTitle.innerText = "Launch Details:"
+    launch.appendChild(launchDetailTitle)
+
+    const launchDetails = document.createElement('p');
+    launchDetails.innerText = allLaunches[0].details
+    launch.appendChild(launchDetails)
+
+    const launchArticle = document.createElement('a');
+    launchArticle.innerText = "Article Link"
+    launchArticle.href = allLaunches[0].links.article
+    launch.appendChild(launchArticle)
+
+    allLaunchContainer.appendChild(launch);
 
 }
 
