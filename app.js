b6850adaf019d68359eff524a8cd7746fbecd4b8
Nasir <nasir-islam@hotmail.com>
2022-03-08 16:35:37 +0000

    load data

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("JS is working");
+
+let allLaunches = [];
+
+
+
+fetch("https://api.spacexdata.com/v5/launches")
+.then(result => result.json())
+.then((data) => allLaunches = data)
+.then(() => console.log(allLaunches[0].flight_number));
+
+
+// console.log(allLaunches);
